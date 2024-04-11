dc641d62855c16fa2442efd8d9732cd8774a6e71
juniormendes <juniormendesjp@gmail.com>
2024-04-10 22:32:22 -0300

    Configuracao inicial do projeto java com spring boot

---
+++ rest-with-spring-boot-and-java/src/test/java/br/com/juniormendes/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.juniormendes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
